3de9dec0520299092f6567c16b646b18cb96d9b8
thisizsudip <sudip.paul.16.3.2005@gmail.com>
2026-01-10 23:50:33 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const CACHE = "contest-tracker-v1";
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
-    "https://raw.githubusercontent.com/username/repo/main/icon.png" // GitHub icon
+    "https://raw.githubusercontent.com/thisizsudip/contest-tracker/main/icon.png"
 ];
 
 // Install event: cache files
@@ -12,24 +12,25 @@ self.addEventListener("install", e => {
     e.waitUntil(
         caches.open(CACHE).then(cache => cache.addAll(FILES_TO_CACHE))
     );
+    self.skipWaiting();
+});
+
+// Activate event: clean old caches
+self.addEventListener("activate", e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.filter(key => key !== CACHE)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+    self.clients.claim();
 });
 
 // Fetch event: serve cached files first
 self.addEventListener("fetch", e => {
     e.respondWith(
         caches.match(e.request).then(res => res || fetch(e.request))
-    );
-});
-
-// Activate event: clean old caches if needed
-self.addEventListener("activate", e => {
-    e.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys
-                    .filter(key => key !== CACHE)
-                    .map(key => caches.delete(key))
-            )
-        )
     );
 });
