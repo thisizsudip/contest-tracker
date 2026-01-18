7af6cd85dfc92d561fbaef47e5d1807b90ef6788
thisizsudip <sudip.paul.16.3.2005@gmail.com>
2026-01-18 19:13:47 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
 const CACHE = "contest-tracker-v1";
 
+// Files to cache
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
     "https://raw.githubusercontent.com/thisizsudip/contest-tracker/main/icon.png"
 ];
 
+// Install event: cache files
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open(CACHE).then(cache => cache.addAll(FILES_TO_CACHE))
@@ -13,6 +15,7 @@ self.addEventListener("install", e => {
     self.skipWaiting();
 });
 
+// Activate event: clean old caches
 self.addEventListener("activate", e => {
     e.waitUntil(
         caches.keys().then(keys =>
@@ -25,6 +28,7 @@ self.addEventListener("activate", e => {
     self.clients.claim();
 });
 
+// Fetch event: serve cached files first
 self.addEventListener("fetch", e => {
     e.respondWith(
         caches.match(e.request).then(res => res || fetch(e.request))
